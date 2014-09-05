23d59cc01e42e9a3dfb25da640c4b7522afe5b5f
Jacob Foster <jacob@britecore.com>
2014-09-04 20:23:54 -0500

    Adding js-py-proto to global libs

--- static/js/external/login.js
+++ static/js/external/login.js
@@ -4,15 +4,18 @@
 
 app.login = {
 
-    username: ko.observable(''),
-    password: ko.observable(''),
-    email: ko.observable(''),
+    loginUsername: ko.observable(''),
+    loginPassword: ko.observable(''),
+
+    registerUsername: ko.observable(''),
+    registerPassword: ko.observable(''),
+    registerEmail: ko.observable(''),
 
     login: function() {
         var self = this;
 
-        var loginUrl = '/external/login?username={0}&password={1}'.format(self.username(), self.password());
-        $.post(loginUrl, function(response) {
+        var loginUrl = '/external/login?username={loginUsername}&password={loginPassword}';
+        $.post(loginUrl.format(ko.toJS(self)), function(response) {
             if (response.success) {
                 console.log('Woot, logged in');
             }
@@ -22,8 +25,8 @@ app.login = {
     register: function() {
         var self = this;
 
-        var registerUrl = '/external/register?username={0}&password={1}&email={2}'.format(self.username(), self.password(), self.email());
-        $.post(registerUrl, function(response) {
+        var registerUrl = '/external/register?username={registerUsername}&password={registerPassword}&email={registerEmail}';
+        $.post(registerUrl.format(ko.toJS(self)), function(response) {
             if (response.success) {
                 console.log('Woot, registered');
             }
