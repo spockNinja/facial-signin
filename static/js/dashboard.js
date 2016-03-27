a74c34c4a7d87af3957014182135bf4f8967b316
Jacob <jacob@britecore.com>
2016-03-27 15:44:13 -0500

    Using webcam.js for taking photos
        TODO need to upload photo and improve UI

---
+++ static/js/dashboard.js
@@ -0,0 +1,7 @@
+Webcam.attach( '#my_camera' );
+
+function take_snapshot() {
+    Webcam.snap( function(data_uri) {
+        document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+    } );
+}
